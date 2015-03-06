5b4cb9e0c0eba9136b0b5e35217ed42124ae0206
mrcorn <mchen300@gmail.com>
2015-03-05 22:16:28 -0800

    Create Display Screen.h

---
+++ Display Screen.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Globals.h"
+#include "MapElement.h"
+#include "Button.h"
+#include <allegro5\allegro_primitives.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
+
+class MapElement;
+
+class DisplayScreen
+{
+private:
+	int xPos;
+	int yPos;
+	float damage;
+	float cooldown;
+	float range;
+	float slow;
+
+	bool allowUpgrade;
+	bool drawRange;
+	bool redLive;
+	bool greenLive;
+	bool blueLive;
+	bool goodToGo;
+	bool loadedFont;
+
+	MapElement grid;
+
+	ALLEGRO_FONT *font15;
+	ALLEGRO_FONT *font11;
+
+public:
+	DisplayScreen();
+	void SetGrid(MapElement *grid); //rename to fit better
+	void SetGrid(int tower, MapElement grid); //rename this to fit better
+	void DrawBackground();
+	void DrawDisplay(Player &player);
+	void ClearDisplay();
+	void Destroy();
+	void LoadFont(ALLEGRO_FONT **font15, ALLEGRO_FONT **font11);
+	void CheckUpgradeValid(MapElement *grid);
+
+	UpgradeRedButton upgradeRed;
+	UpgradeGreenButton upgradeGreen;
+	UpgradeBlueButton upgradeBlue;
+};
