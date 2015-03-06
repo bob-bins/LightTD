e719072839871ebbf40885b02c276ea098afc225
mrcorn <mchen300@gmail.com>
2015-03-05 22:16:07 -0800

    Create Globals.h

---
+++ Globals.h
@@ -0,0 +1,7 @@
+#pragma once
+
+const int MAP_WIDTH = 690; //must be multiple of COORD_BOX_SIZE
+const int MAP_HEIGHT = 600; //must be multiple of COORD_BOX_SIZE
+const int COORD_BOX_SIZE = 30;
+const int DISP_WIDTH = MAP_WIDTH + 210;
+const int DISP_HEIGHT = MAP_HEIGHT;
