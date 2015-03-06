d3102cd0ee56287963fd3abdd99ed34505d1f4ca
mrcorn <mchen300@gmail.com>
2015-03-05 22:14:19 -0800

    Create Projectile.cpp

---
+++ Projectile.cpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "Projectile.h"
+#include <math.h>
+#include <allegro5\allegro_primitives.h>
+
+Projectile::Projectile()
+{
+	live = false;
+	monsterID = 0;
+	damage = 10;
+	speed = 4;
+	slow = 0;
+}
+float Projectile::CollideMonster(int mx, int my)
+{
+	if(abs(xCenter - mx) < 3 && abs(yCenter - my) < 3)
+	{
+		live = false;
+		return damage;
+	}
+	return 0;
+}
+void Projectile::DrawProjectile()
+{
+	al_draw_circle(xCenter, yCenter, 5, al_map_rgb(255, 255, 255), 1);
+}
+void Projectile::UpdatePosition(int mx, int my)
+{
+	float xinc = mx-xCenter;
+	float yinc = my-yCenter;
+	float hypot = sqrt(pow(xinc, 2)+pow(yinc, 2));
+	xCenter = xCenter + xinc/hypot*speed;
+	yCenter = yCenter + yinc/hypot*speed;
+}
